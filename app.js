a82a927e9db76dc3aeedde4eebfba13fb463e7d4
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-09-13 08:06:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Created by Tivotal */
+
+let card = document.querySelector(".card");
+
+card.onmousemove = (e) => {
+  let x = e.pageX - card.offsetLeft;
+  let y = e.pageY - card.offsetTop;
+
+  card.style.setProperty("--x", x + "px");
+  card.style.setProperty("--y", y + "px");
+};
